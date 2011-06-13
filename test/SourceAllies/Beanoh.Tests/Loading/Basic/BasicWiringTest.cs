0661b3987b1f1fcd968f4bf3c994b3f1f0a080d9
Akrem Saed <akrem.saed@gmail.com>
2011-06-13 17:56:46 -0500

    renaming to BasicWiringTest

---
+++ test/SourceAllies/Beanoh.Tests/Loading/Basic/BasicWiringTest.cs
@@ -0,0 +1,39 @@
+﻿#region License
+/* 
+ * Copyright (c) 2011 Source Allies
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 3.0.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, please visit
+ * http://www.gnu.org/licenses/lgpl-3.0.txt.
+*/
+#endregion
+
+#region Imports
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+#endregion
+
+namespace SourceAllies.Beanoh.Loading.Basic
+{
+    [TestFixture]
+   public class BasicWiringTest : BeanohTestCase 
+    {
+        [Test]
+        public void TestWiring()
+        {
+            AssertContextLoading();
+        }
+    }
+}

03015de603d4587bcda44fcc74a724a228173a3d
Akrem Saed <akrem.saed@gmail.com>
2012-06-19 22:10:01 -0500

    creating the nuget package with symbols

--- src/SourceAllies/Beanoh/AssemblyInfo.cs
+++ src/SourceAllies/Beanoh/AssemblyInfo.cs
@@ -6,7 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Beanoh")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Beanoh is a simple open source way to verify you Spring context. Teams that leverage Beanoh spend less time focusing on configuring Spring and more time adding business value")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Akrem Saed")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -26,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
